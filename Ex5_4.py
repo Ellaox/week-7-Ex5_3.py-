b4392f42f9e482bf939169b6e4284a74287adfac
Ellaox <40073721+Ellaox@users.noreply.github.com>
2019-03-21 12:46:08 +0000

    Create Ex5_4.py

---
+++ Ex5_4.py
@@ -0,0 +1,18 @@
+# Counting3.py
+# a program using a for loop and the range function
+# to display on the screen the numbers 10 to 20
+# D.H. 5th January 2005
+
+print "A count from 5 to 15" 
+
+# range is called with two arguments
+# a start number and an end number(up to but not included)
+
+for i in range(5,15):
+    print i,
+    
+  >>> ================================ RESTART ================================
+>>> 
+A count from 5 to 15
+5 6 7 8 9 10 11 12 13 14
+>>>   
